4495016c05ed5268026a42223ea8de60a526e5a3
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-10-26 19:40:46 -0500

    Update certifications.php

--- certifications.php
+++ certifications.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title style="background-color:powderblue; text-align:center;">Certifications</title>
+    <title>Certifications</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
     
-      <h1>Certifications</h1>
+      <h1 style="background-color:powderblue; text-align:center;">CERTIFICATIONS</h1>
       <table class="table table-striped">
         <thead>
           <tr>
